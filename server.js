0ad96f319a1fe2559ccfc4a020a778845bf783fd
amirhossein gilani nezhad <amirclown49@gmail.com>
2023-04-13 06:50:45 -0700

    add post to the project

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.post("/mamad", (req, res) => {
+  const num1 = req.body.num1;
+  const num2 = req.body.num2;
+  //res.status(200).json({
+  // result: num1 * num2,
+  //});
+  res.send(`result is ${num1 * num2}`);
 });
 
 app.listen(port, () => {
